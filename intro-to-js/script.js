0e20b9b32802f41037999dc08c47a0575bf2cda8
Sarah <sarahdavis398@gmail.com>
2022-05-02 16:30:09 +1000

    Introduction to Javascript completed

--- intro-to-js/script.js
+++ intro-to-js/script.js
@@ -1,20 +1,31 @@
-window.onload = function() {
+window.onload = function () {
     // start
+    let starterDiv = document.getElementById('starter-div');
+    let firstHeading = document.createElement('h2');
+    firstHeading.innerText = 'This is an H2 created with JS';
+    firstHeading.classList.add('js-header');
+    starterDiv.appendChild(firstHeading);
 
+    let secondHeading = document.createElement('h2');
+    secondHeading.innerText = 'This is another H2 created with JS';
+    secondHeading.classList.add('js-header');
+    secondHeading.id = 'second-h2';
+    starterDiv.appendChild(secondHeading);
     // end
 }
 
 
-function showName(){
+function showName() {
     console.log('showName');
     // start
-
+    let name = document.getElementById('name').value;
+    document.getElementById('greeting-text').innerText = 'Hi ' + name;
     // end
 }
 
 function changeColour(colour) {
     // start
-
+    document.body.style.backgroundColor = colour;
     // end
 }
 
@@ -24,6 +35,7 @@ function resetColour() {
 
 function toggleLightbulb() {
     // start
-
+    let lightbulb = document.getElementById('light');
+    lightbulb.classList.toggle("on");
     // end
 }
